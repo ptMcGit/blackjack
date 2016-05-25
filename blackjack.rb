94734bab267e13e82c015d4fd85c556c3797ff09
Zach H <zdhadgraft@gtcc.edu>
2016-05-25 16:18:25 -0400

    Switched to James's file and made modifications.

--- blackjack.rb
+++ blackjack.rb
@@ -1,103 +1,78 @@
-
-class Blackjack
-  # maybe we want subclasses?
-end
-
 class Card
+  attr_reader :rank, :suit
 
   def initialize rank, suit
-    @value = rank
-    @suit = suit
-    @to_s = rank.to_s + suit.to_s
+    @rank, @suit = rank, suit
   end
 
-  attr_reader :suit, :to_s
-
   def value
-    if [:K, :Q, :J].include? @value
-      return 10
+    case rank
+    when :K, :Q, :J
+      10
+    when :A
+      1
     else
-      return @value
+      rank
     end
   end
 
+  def to_s
+    "#{rank}#{suit}"
+  end
 end
 
 class Deck
+  SUITS = [:S, :D, :H, :C]
+  RANKS = 2.upto(10).to_a + [:J, :Q, :K, :A]
+
+  attr_reader :cards, :drawn
 
   def initialize
-    @cards = (1..52).to_a
-    @drawn = []
+    @cards, @drawn = [], []
+    SUITS.each do |suite|
+      RANKS.each do |rank|
+        @cards.push Card.new(suite, rank)
+      end
+    end
+    @cards.shuffle!
   end
-
-  attr_reader :cards
 
   def draw
-    @drawn.push @cards.pop
-    @drawn.last
-  end
-
-  def drawn
-    @drawn
+    card = @cards.pop
+    @drawn.push card
+    card
   end
 end
 
 class Hand
+  attr_reader :cards
 
   def initialize
-    @value = []
-    @to_s = []
+    @cards = []
   end
 
-  def add *card
-    card.each do |x|
-      @value.push x.value
-      @to_s.push x.to_s
-    end
+  def add *new_cards
+    @cards += new_cards
   end
 
   def value
-    count = 0
-
-    0.upto @value.length do
-      if [:K, :J, :Q].include? @value[count]
-        @value[count] = 10
-      end
-      count += 1
+    base = cards.map { |c| c.value }.reduce(:+)
+    if cards.any? { |c| c.rank == :A } && base <= 11
+      base + 10
+    else
+      base
     end
-
-    @value.map! { |x| x.to_s }.sort
-    @value.sort!
-
-    count = 0
-
-    final_value = 0
-
-    0.upto @value.length do
-      if @value[count] == "A"
-        if final_value > 10
-          final_value += 1
-        else
-          final_value += 11
-        end
-      else
-        final_value += @value[count].to_i
-      end
-      count += 1
-    end
-    final_value
-  end
-
-  def  busted?
-    value > 21
   end
 
   def blackjack?
     value == 21
   end
 
-  def to_s
-    @to_s.join(", ")
+  def busted?
+    value > 21
   end
 
+  def to_s
+    cards.map { |c| c.to_s }.join ", "
+  end
 end
