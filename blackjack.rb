7a04b17ab4c0f3419c021d420d40610484037884
Zach H <zdhadgraft@gtcc.edu>
2016-05-24 22:23:50 -0400

    Passed all tests.

--- blackjack.rb
+++ blackjack.rb
@@ -1,15 +1,17 @@
 
-
 class Blackjack
   # maybe we want subclasses?
 end
 
 class Card
 
-  def initialize rank, symbol
+  def initialize rank, suit
     @value = rank
-    @symbol = symbol
+    @suit = suit
+    @to_s = rank.to_s + suit.to_s
   end
+
+  attr_reader :suit, :to_s
 
   def value
     if [:K, :Q, :J].include? @value
@@ -22,6 +24,7 @@ class Card
 end
 
 class Deck
+
   def initialize
     @cards = (1..52).to_a
     @drawn = []
@@ -37,4 +40,64 @@ class Deck
   def drawn
     @drawn
   end
+end
+
+class Hand
+
+  def initialize
+    @value = []
+    @to_s = []
+  end
+
+  def add *card
+    card.each do |x|
+      @value.push x.value
+      @to_s.push x.to_s
+    end
+  end
+
+  def value
+    count = 0
+
+    0.upto @value.length do
+      if [:K, :J, :Q].include? @value[count]
+        @value[count] = 10
+      end
+      count += 1
+    end
+
+    @value.map! { |x| x.to_s }.sort
+    @value.sort!
+
+    count = 0
+
+    final_value = 0
+
+    0.upto @value.length do
+      if @value[count] == "A"
+        if final_value > 10
+          final_value += 1
+        else
+          final_value += 11
+        end
+      else
+        final_value += @value[count].to_i
+      end
+      count += 1
+    end
+    final_value
+  end
+
+  def  busted?
+    value > 21
+  end
+
+  def blackjack?
+    value == 21
+  end
+
+  def to_s
+    @to_s.join(", ")
+  end
+
 end
